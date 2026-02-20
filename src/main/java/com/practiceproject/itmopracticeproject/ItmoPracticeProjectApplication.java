33e8c54b6c1a2868ddd633873f672f777bad7af6
GAIshnik02 <jurazakhmatov@icloud.com>
2026-02-20 18:38:23 +0300

    init commit

---
+++ src/main/java/com/practiceproject/itmopracticeproject/ItmoPracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.practiceproject.itmopracticeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItmoPracticeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItmoPracticeProjectApplication.class, args);
+    }
+
+}
